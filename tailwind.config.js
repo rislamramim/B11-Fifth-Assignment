71cfc2eaa0fef432764e1af5c8b46adb05836b35
rislamramim <ramimrislam2002@gmail.com>
2025-03-02 03:09:00 +0600

    Completed all the question ans answer for second html file,clear the tailwind file and added

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-1. Selecting by ID.EX.-- document.getElementById(id).
-
-2. Selecting by Class Name.Ex.-- document.getElementsByClassName(className).
-
-3. Selecting by Tag Name.Ex.-- document.getElementsByTagName(tagName).
-
-4. Using CSS Selectors.Ex.-- document.querySelector(selector) and document.querySelectorAll(selector)
