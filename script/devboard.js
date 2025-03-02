ed1a74087dfd368862f836973408d19af3868b99
rislamramim <ramimrislam2002@gmail.com>
2025-03-02 13:40:20 +0600

    Added full java script for requirment task

--- script/devboard.js
+++ script/devboard.js
@@ -65,6 +65,73 @@ document.getElementById('job-button').addEventListener('click',function(event){
     jobButton.classList.add('disabled');    
 })
 
+
+
+document.getElementById('fix-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const remainWork = document.getElementById('remain-work').innerText;
+    const convertedRemainWork = parseInt(remainWork);
+    const sum = convertedRemainWork - 1;
+    document.getElementById("remain-work").innerText = sum;
+
+})
+document.getElementById('dark-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const remainWork = document.getElementById('remain-work').innerText;
+    const convertedRemainWork = parseInt(remainWork);
+    const sum = convertedRemainWork - 1;
+    document.getElementById("remain-work").innerText = sum;
+
+})
+document.getElementById('home-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const remainWork = document.getElementById('remain-work').innerText;
+    const convertedRemainWork = parseInt(remainWork);
+    const sum = convertedRemainWork - 1;
+    document.getElementById("remain-work").innerText = sum;
+
+})
+document.getElementById('emoji-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const remainWork = document.getElementById('remain-work').innerText;
+    const convertedRemainWork = parseInt(remainWork);
+    const sum = convertedRemainWork - 1;
+    document.getElementById("remain-work").innerText = sum;
+
+})
+document.getElementById('openai-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const remainWork = document.getElementById('remain-work').innerText;
+    const convertedRemainWork = parseInt(remainWork);
+    const sum = convertedRemainWork - 1;
+    document.getElementById("remain-work").innerText = sum;
+
+})
+document.getElementById('job-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const remainWork = document.getElementById('remain-work').innerText;
+    const convertedRemainWork = parseInt(remainWork);
+    const sum = convertedRemainWork - 1;
+    document.getElementById("remain-work").innerText = sum;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // linkup with two html file
 document.getElementById('discover').addEventListener('click',function(event){
     event.preventDefault();
